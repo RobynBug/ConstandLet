08be680aaa488a6c18bcbfb4852dde9263f0f71e
Robyn <neiya648@gmail.com>
2024-02-11 03:15:42 -0500

    completed exercise

---
+++ LetandConstExercise.js
@@ -0,0 +1,23 @@
+// var PI = 3.14;
+// PI = 42; // stop me from doing this!
+
+/* Write an ES2015 Version */
+const PI = 3.14;
+
+/* What is the difference between var and let?
+
+var declares a variable that is hoisted and let is a block declartion
+
+*/
+
+/* What is the difference between var and const?
+
+var can be reassigned but const cannot.
+
+What is the difference between let and const?
+let can be reassigned but const cannot.
+
+What is hoisting?
+Hoisting a variablle makes the variable a global variable that can be accessed throughout the program.
+
+*/
